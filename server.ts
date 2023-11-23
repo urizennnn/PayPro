d670c00d26bbec5944e4def7648d682bcc48db99
urizennnn <igamerryt@gmail.com>
2023-11-23 14:23:02 +0100

    added default route

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import { ConnectSQL } from './Database/SQLconnection';
@@ -10,24 +10,18 @@ const PORT = process.env.PORT || 5000;
 server.use(express.json());
 server.use(cookieParser(process.env.JWT_SECRET));
 server.use(morgan('dev'));
-
-// Your other middleware and routes
 server.use('/user', Registrouter);
 
-// Error handler middleware
-server.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+server.all('/', (req: Request, res: Response) => {
+  res.send('Working');
 });
 
 (async () => {
   try {
      ConnectSQL();
-    
     const app = server.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
-
     process.on('SIGINT', () => {
       console.log('Shutting down...');
       app.close(() => {
