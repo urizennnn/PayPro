1ee873411b2a6a73f891f3448b9b78486bc60117
urizennnn <igamerryt@gmail.com>
2023-11-23 19:24:20 +0100

    fixing bugs

--- controllers/signUp.ts
+++ controllers/signUp.ts
@@ -1,23 +1,22 @@
 import { Request, Response } from "express";
-import { generateRefreshToken, insertData } from "../utils/helper";
+import { insertData,generateRefreshToken } from "../utils/helper";
 
 export const SignUser = async (req: Request, res: Response) => {
-  try {
-    const { Email, BName, Password }: Record<string, string> = req.query as Record<string, string>;
+    try {
+        const { Email, BName, Password }: Record<string, string> = req.query as Record<string, string>;
 
-    if (!Email || !BName || !Password) {
-      return res.status(400).json({ success: false, message: 'Bad Request: Missing required parameters' });
+        if (!Email || !BName || !Password) {
+            return res.status(400).json({ success: false, message: 'Bad Request: Missing required parameters' });
+        }
+
+        const date: string = new Date().toISOString().split('T')[0].replace(/-/g, '/');
+        const Vtoken: string = generateRefreshToken();
+
+        await insertData(Email, BName, date, Vtoken, Password);
+
+        res.status(200).json({ success: true, message: 'User signed up successfully' });
+    } catch (error: any) {
+        console.error('Error signing up user:', error);
+        res.status(500).json({ success: false, error: error.message });
     }
-
-   const date = new Date().toISOString().split('T')[0];
-
-    const Vtoken: string = generateRefreshToken();
-
-    await insertData(Email, BName, date, Vtoken, Password);
-
-    res.status(200).json({ success: true, message: 'User signed up successfully' });
-  } catch (error: any) {
-    console.error('Error signing up user:', error);
-    res.status(500).json({ success: false, error: error.message });
-  }
 };
--- utils/helper.ts
+++ utils/helper.ts
@@ -1,7 +1,7 @@
 import { pool } from "../Database/SQLconnection";
-import crypto from 'crypto'
+import crypto from 'crypto';
 
-export function queryAsync(sql: string, values: any[]): Promise<any> {
+export function queryAsync(sql: string, values: any[]): Promise<void> {
     return new Promise((resolve, reject) => {
         pool.getConnection((err, connection) => {
             if (err) {
@@ -9,8 +9,8 @@ export function queryAsync(sql: string, values: any[]): Promise<any> {
                 return;
             }
 
-            connection.query(sql, values, (error:any, results:any) => {
-                connection.release(); 
+            connection.query(sql, values, (error: any, results: any) => {
+                connection.release();
                 if (error) {
                     reject(error);
                 } else {
@@ -21,22 +21,19 @@ export function queryAsync(sql: string, values: any[]): Promise<any> {
     });
 }
 
-
 export function createVerificationToken(): string {
-  return crypto.randomBytes(40).toString("hex");
+    return crypto.randomBytes(40).toString("hex");
 }
+
 export function generateRefreshToken(): string {
-  return crypto.randomBytes(40).toString("hex");
+    return crypto.randomBytes(40).toString("hex");
 }
 
-export async function insertData(Email: any, Bname: string,date:string,token:string,special:string) {
-   
-
+export async function insertData(Email: any, Bname: string, date: string, token: string, special: string) {
     const insertQuery = `INSERT INTO ${process.env.TABLE}(${process.env.PRI}, ${process.env.Token}, ${process.env.Date},${process.env.Name},${process.env.Unique}) VALUES (?, ?,?, ?,?);`;
     try {
-        const result = await queryAsync(insertQuery, [Email, Bname, token,date,Bname,special]);
+        const result = await queryAsync(insertQuery, [Email, Bname, token, date, Bname, special]);
         console.log('Data inserted successfully:', result);
-
     } catch (error: any) {
         if (error.message.includes('Out of range value for column')) {
             console.error(`Error inserting data: ${error.message}.`);
