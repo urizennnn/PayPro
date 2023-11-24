2772c5203e293d0672c8f69d00409c31791e11b0
urizennnn <igamerryt@gmail.com>
2023-11-24 20:20:58 +0100

    added login routes

--- controllers/login.ts
+++ controllers/login.ts
@@ -14,7 +14,8 @@ if(!user){
 const check:any = await LoginUser(Email)
 
 if(check === Password){
-    res.status(200).json({'success':true,'message':"User logged In"})}
+    res.status(200).json({'success':true,'message':"User logged In","user":user})
+    }
 
 }catch(error:any){
     console.error('Error signing up user:', error);
